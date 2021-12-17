c03ce99ec13bfb189d33a4d91569ad65c2357ebb
Julián Perna <julianperna@gmail.com>
2021-12-17 14:07:10 -0300

    Updates

---
+++ precache-manifest.de8255e85263808de92375fda4cfa0d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f7f59739052edf25c948c052a7f7df",
+    "url": "/sitiodecompras/index.html"
+  },
+  {
+    "revision": "c807eb2be555cec9d46d",
+    "url": "/sitiodecompras/static/css/2.eacbdd6c.chunk.css"
+  },
+  {
+    "revision": "af6227685d893e085a08",
+    "url": "/sitiodecompras/static/css/main.faba022c.chunk.css"
+  },
+  {
+    "revision": "c807eb2be555cec9d46d",
+    "url": "/sitiodecompras/static/js/2.7807d435.chunk.js"
+  },
+  {
+    "revision": "af6227685d893e085a08",
+    "url": "/sitiodecompras/static/js/main.1f70dc30.chunk.js"
+  },
+  {
+    "revision": "e0d8ff58c89516a4a049",
+    "url": "/sitiodecompras/static/js/runtime~main.ce99052e.js"
+  }
+]);
